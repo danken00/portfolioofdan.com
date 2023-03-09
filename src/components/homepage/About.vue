f4eb8bd197ffe9eb741f9d6692e5542f2200434a
Dan Kennedy <dan@mojoplease.com>
2023-03-09 14:50:20 +0400

    Completed header, generated images

---
+++ src/components/homepage/About.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row">
+    <div class="col-12 about">
+      <div class="photo">ad wad ad</div>
+      <div class="about-list"><h3>awdawd </h3></div>
+      <div class="about-list"><h3>awdawd </h3></div>
+      <div class="key-techologies">ad awd a</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  props: {
+    // msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.about {
+  z-index: 10;
+  background-color: white;
+  box-shadow: 0 0 18px rgba(0, 0, 0, 0.2);
+}
+</style>
---
+++ src/components/homepage/Header.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="row">
+    <div class="col-12 header background-gradient">
+      <div class="header-images">
+        <img :src="require('@/assets/images/homepage/dan.png')" class="image-dan">
+        <img :src="require('@/assets/images/homepage/slashes-truncated.png')" class="image-slashes">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    // msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.header {
+  min-height: 180px;
+  overflow: hidden;
+  position: relative;
+
+  .header-images {
+    height: 100px;
+    position: absolute;
+    right: 30px;
+    bottom: -42px;
+
+    img.image-dan {
+      width: 179px;
+      opacity: 85%;
+      position: absolute;
+      top: 0px;
+      right: 0px;
+    }
+
+    img.image-slashes {
+      opacity: 30%;
+      position: absolute;
+      top: 0px;
+      right: 184px;
+    }
+  }
+}
+
+.background-gradient {
+  background-image: linear-gradient(to left, #10c3d8, #55d2ae);
+}
+
+
+</style>
