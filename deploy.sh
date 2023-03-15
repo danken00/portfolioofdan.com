7482438e25bafac56332c1c2e0a39fd8ed9daf25
Dan Kennedy <dan@mojoplease.com>
2023-03-15 09:44:55 +0400

    Fine-tuned messaging on deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env sh
 
-echo "Are you sure? Have you committed your work? This will delete anything not committed!"
+echo "Have you committed your work? This will delete anything not committed!"
 echo " "
 read -p "y/n: " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+  echo " "
+  echo "Let's go!"
+  echo " "
   set -e
   git checkout --orphan gh-pages
   npm run build
